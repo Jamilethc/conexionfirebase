9708ed50341e00eb6876ee79cfaad517cc36169b
Roxana-Rm <roxana.escobar22@itca.edu.sv>
2023-11-16 20:47:45 -0600

    registrar pacientes

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -28,6 +29,7 @@ class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
 
   final List<Widget> _widgetOptions = [
+    PaginaInicio(),
     IngresarPacientes(),
     IngresarDatosPaciente(),
     Page3(),
@@ -58,15 +60,23 @@ class _MyHomePageState extends State<MyHomePage> {
                 'Menu',
                 style: TextStyle(
                   color: Colors.white,
-                  fontSize: 24,
+                  fontSize: 30,
                 ),
               ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Inicio'),
+              onTap: () {
+                _onItemTapped(0);
+                Navigator.pop(context);
+              },
             ),
             ListTile(
               leading: Icon(Icons.people),
               title: Text('Ingresar pacientes'),
               onTap: () {
-                _onItemTapped(0);
+                _onItemTapped(1);
                 Navigator.pop(context);
               },
             ),
@@ -74,7 +84,7 @@ class _MyHomePageState extends State<MyHomePage> {
               leading: Icon(Icons.assignment),
               title: Text('Ingresar datos del paciente'),
               onTap: () {
-                _onItemTapped(1);
+                _onItemTapped(2);
                 Navigator.pop(context);
               },
             ),
@@ -82,7 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
               leading: Icon(Icons.description),
               title: Text('Historial del paciente'),
               onTap: () {
-                _onItemTapped(2);
+                _onItemTapped(3);
                 Navigator.pop(context);
               },
             ),
@@ -93,11 +103,87 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class IngresarPacientes extends StatelessWidget {
+class PaginaInicio extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Contenido de Ingresar Pacientes'),
+      child: Text('Contenido de la Página de Inicio'),
+    );
+  }
+}
+
+class IngresarPacientes extends StatefulWidget {
+  @override
+  _IngresarPacientesState createState() => _IngresarPacientesState();
+}
+
+class _IngresarPacientesState extends State<IngresarPacientes> {
+  final TextEditingController _nombreController = TextEditingController();
+  final TextEditingController _edadController = TextEditingController();
+
+  // Referencia a la colección 'pacientes' en Firestore
+  final CollectionReference _pacientesCollection =
+      FirebaseFirestore.instance.collection('pacientes');
+
+  void _registrarPaciente() async {
+    // Obtener los valores del formulario
+    String nombre = _nombreController.text;
+    int edad = int.tryParse(_edadController.text) ??
+        0; // Convertir a entero, o usar 0 si no se puede
+
+    // Validar que el nombre no esté vacío
+    if (nombre.isNotEmpty) {
+      // Guardar el paciente en Firestore
+      await _pacientesCollection.add({
+        'nombre': nombre,
+        'edad': edad,
+      });
+
+      // Limpiar los campos después de guardar
+      _nombreController.clear();
+      _edadController.clear();
+
+      // Mostrar un mensaje de éxito
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Paciente registrado con éxito')),
+      );
+    } else {
+      // Mostrar un mensaje de error si el nombre está vacío
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Por favor, ingrese el nombre del paciente')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Registrar Paciente',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 16),
+          TextField(
+            controller: _nombreController,
+            decoration: InputDecoration(labelText: 'Nombre del Paciente'),
+          ),
+          SizedBox(height: 16),
+          TextField(
+            controller: _edadController,
+            decoration: InputDecoration(labelText: 'Edad del Paciente'),
+            keyboardType: TextInputType.number,
+          ),
+          SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: _registrarPaciente,
+            child: Text('Registrar Paciente'),
+          ),
+        ],
+      ),
     );
   }
 }
